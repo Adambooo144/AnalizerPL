579333ba38e3d041dcbf0e95dbdfa4258e013479
Adambooo144 <adambooo144@gmail.com>
2025-07-26 22:21:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,20 @@ else:
 
     dyscyplina = st.selectbox("Wybierz dyscyplinę", lista_dyscyplin)
     kraj = st.selectbox("Wybierz kraj", lista_krajow)
-    liga = st.selectbox("Wybierz ligę", ["Ekstraklasa", "I liga", "II liga"])
+    # Liga dostępna tylko, jeśli wybrano Polska
+if kraj == "Polska":
+    lista_lig = ["Ekstraklasa", "I liga", "II liga"]
+elif kraj == "Hiszpania":
+    lista_lig = ["La Liga"]
+else:
+    lista_lig = []
+
+if lista_lig:
+    liga = st.selectbox("Wybierz ligę", lista_lig)
+else:
+    st.info("Brak dostępnych lig dla wybranego kraju.")
+    liga = None  # opcjonalnie
+
 
     druzyna1 = st.text_input("Drużyna 1")
     druzyna2 = st.text_input("Drużyna 2")
